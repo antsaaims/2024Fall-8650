0c60b66d825469ede58879d435d481aca96e798a
Antsa Tantely Fandresena Rakotondrafara <62647980+antsaaims@users.noreply.github.com>
2024-11-14 17:27:57 -0500

    Update linkedvsdynamic.cpp

--- cpp-matrix-speed/linkedvsdynamic.cpp
+++ cpp-matrix-speed/linkedvsdynamic.cpp
@@ -1,7 +1,7 @@
 #include <cmath>
 #include <iostream>
 #include <vector>
-
+#include <list>
 #include "timer.h"
 // Fill a linked list
 void
@@ -12,13 +12,13 @@ fill_linked(std::list<double> &myList)
       myList.push_back(i);
     }
 }
+
 // compute the the sum of all entries by looping over them
 double
 sum_entries_linked(std::list<double> &myList)
 {
   double result = 0.0;
-  double result = 0;
-    for (double n : l)
+    for (double n : myList)
       result += n;
   return result;
 }
@@ -31,37 +31,68 @@ test_linked()
   Timer<> clock; // Timer is defined in timer.h in the same folder
   // Let's time it:
   clock.tick();
-  fill(myList);
+  fill_linked(myList);
   clock.tock();
-  std::cout << "N=" << size
-            << " mem: " << std::round(bytes / 1024 / 1024 * 100) / 100 << "mb"
-            << " Mflops/s: " << std::round(flops / secs / 1000000)
-            << " mem: " << std::round(bytes / secs / 1024 / 1024) << "mb/s"
-            << " took " << secs << "s"
-            << " runs: " << runs << " r=" << r << std::endl;
-            
-  int    runs = 1000000 / size;
-  double r    = 1.0;
+  const double secs  = clock.duration().count() / 1000.0; //the timer gives milliseconds
+
+  std::cout << "Filling the linked list,  took " << secs << "s.\n";
+  //Getting the sum of all entries
+  clock.tick();
+  int    runs = 100;
+  double r    = 0.0;
   for (int i = 0; i < runs; ++i)
-    r += frob(mat);
-  
-
-  const double secs  = clock.duration().count() / 1000.0 / runs;
-  const double bytes = 1.0 * sizeof(double) * size * size;
-  const double flops = 3.0 * size * size;
-
-  std::cout << "N=" << size
-            << " mem: " << std::round(bytes / 1024 / 1024 * 100) / 100 << "mb"
-            << " Mflops/s: " << std::round(flops / secs / 1000000)
-            << " mem: " << std::round(bytes / secs / 1024 / 1024) << "mb/s"
-            << " took " << secs << "s"
-            << " runs: " << runs << " r=" << r << std::endl;
+    r += sum_entries_linked(myList);
+  clock.tock();
+  double sec2  = clock.duration().count() / 1000.0 / runs;
+  std::cout << "Compute the sum of all entries of a linked list by looping over them took " << sec2 << "s.\n";
+}
+//Now for std::vector
+void
+fill_vector(std::vector<double> &myVect)
+{
+  for (int i = 0; i < 1000000; ++i)
+    {
+      myVect.push_back(i);
+    }
 }
 
+// compute the the sum of all entries by looping over them
+double
+sum_entries_vector(std::vector<double> &myVect)
+{
+  double result = 0.0;
+    for (double n : myVect)
+      result += n;
+  return result;
+}
+
+void
+test_vector()
+{
+  std::vector<double> myVect;
+  // Filling and timin
+  Timer<> clock; // Timer is defined in timer.h in the same folder
+  // Let's time it:
+  clock.tick();
+  fill_vector(myVect);
+  clock.tock();
+  const double sec  = clock.duration().count() / 1000.0; //the timer gives milliseconds
+
+  std::cout << "Filling the std::vector,  took " << sec << "s.\n";
+  //Getting the sum of all entries
+  clock.tick();
+  int    runs = 100;
+  double r    = 0.0;
+  for (int i = 0; i < runs; ++i)
+    r += sum_entries_vector(myVect);
+  clock.tock();
+  double secs  = clock.duration().count() / 1000.0 / runs;
+  std::cout << "Compute the sum of all entries of the std:vector by looping over them took " << secs << "s.\n";
+}
 int
 main()
 {
-  std::cout << "Using linked list, here are the result";
   test_linked();
+  test_vector();
   return 1;
 }
